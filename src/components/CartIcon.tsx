8561f72fbf43bfbe80b77c99820cc6f90627bbce
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-12-01 02:21:51 +0200

    update

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-const CartIcon = () => {
-  return (
-    <Link href="/cart" className="flex items-center gap-4">
-      <div className="relative w-8 h-8 md:h-5 md:w-5">
-        <Image src="/cart.png" alt="cart image" fill />
-      </div>
-      <span>Cart (3)</span>
-    </Link>
-  );
-};
+const CartIcon = () => (
+  <Link href="/cart" className="flex items-center gap-4">
+    <div className="relative w-8 h-8 md:h-5 md:w-5">
+      <Image src="/cart.png" alt="cart image" fill />
+    </div>
+    <span>Cart (3)</span>
+  </Link>
+);
 
 export default CartIcon;
