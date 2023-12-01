491f24d5f27500dcb4300ac642a86bc7ef0b13b9
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2023-12-01 02:16:44 +0200

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ const LogingPage = () => {
   return (
     <div className="p-4 flex items-center justify-center h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
       {/* box */}
-      <div className="h-[90%] shadow-2xl rounded-md flex flex-col md:flex-row w-[80%] md:h-[70%]  lg:w-[70%] 2xl:w-1/2">
+      <div className="h-[80%] shadow-2xl rounded-md flex flex-col md:flex-row w-[80%] md:h-[60%]  lg:w-[70%] 2xl:w-1/2">
         {/* image */}
         <div className="relative h-1/3 w-full md:h-full md:w-1/2 ">
           <Image
@@ -18,7 +18,7 @@ const LogingPage = () => {
           />
         </div>
         {/* form */}
-        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+        <div className="p-12 flex flex-col gap-8 md:w-1/2">
           <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
           <p>Log into youe account or create a new one using social buttons</p>
           <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
