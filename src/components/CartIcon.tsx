01ad0b2d3fdb7f45dd976a3e9bf7690bf74ebd10
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2024-04-10 11:26:29 +0300

    update cart icon count font

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -13,13 +13,13 @@ const CartIcon = () => {
 
   return (
     <Link href="/cart" className="flex items-center  gap-2 relative">
-      <div className="relative w-5 h-5 md:h-7 md:w-7 md:bottom-[3px]">
+      <div className="relative w-5 h-5 md:h-6 md:w-6 md:bottom-[3px]">
         <Image src="/cart.png" alt="cart image" fill />
       </div>
       {/* <span>{totalItems > 0 ? totalItems : null}</span> */}
       {totalItems === 0 || (
-        <div className=" absolute bg-green-500 w-4 h-4 md:p-3 left-3 md:left-5 bottom-3 flex rounded-full justify-center text-white items-center text-xs md:text-sm">
-          {totalItems}
+        <div className=" absolute  bg-green-500 w-4 h-4 md:p-2 left-3 md:left-5 bottom-3 flex rounded-full justify-center text-white items-center text-xs md:text-xs">
+          <span className="assistant-regular">{totalItems}</span>
         </div>
       )}
     </Link>
