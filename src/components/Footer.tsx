3279b8670d86aae04d0f608e0b745e5d8dac6c97
vlad medvede <vladmedvede@vlads-MacBook-Air.local>
2024-04-15 22:16:03 +0300

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const Footer = () => (
-  <div className="h-12 md:h-24 px-4 lg:px-20 xl:p-40 text-xs md:text-base xl:text-xl text-red-500 flex items-center justify-between">
+  <div className="h-12 md:h-20 px-4 lg:px-12  text-xs md:text-base xl:text-lg text-red-500 flex items-center justify-between">
     <Link className="anton-regular" href="/">
       Foody
     </Link>
