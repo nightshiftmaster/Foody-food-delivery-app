7ac1620e54886cf967b639a6b2ad5e84b4fe271f
nightshiftmaster <nightshiftmaster@gmail.com>
2024-05-13 22:14:13 +0300

    tests config

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,10 +38,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Firefox"] },
-    },
+    // {
+    //   name: "firefox",
+    //   use: { ...devices["Firefox"] },
+    // },
 
     {
       name: "webkit",
